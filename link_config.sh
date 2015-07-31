15a164ea38a5505c17e976135a99b69c18be041f
Gary Johnson <garycjohnso@gmail.com>
2015-07-31 00:57:31 -0400

    force link creation with -f

--- link_config.sh
+++ link_config.sh
@@ -3,9 +3,9 @@
 DOT_FILES="$HOME/.dotfiles"
 
 ### Link Bash Files & Misc Config Files
-mkdir "$HOME/.atom/"
-ln -s "$DOT_FILES/bash/bash_profile" "$HOME/.bash_profile"
-ln -s "$DOT_FILES/bash/bashrc" "$HOME/.bashrc"
-ln -s "$DOT_FILES/editorconfig/editorconfig" "$HOME/.editorconfig"
-ln -s "$DOT_FILES/atom/config.cson" "$HOME/.atom/config.cson"
-ln -s "$DOT_FILES/ansible/ansible.cfg" "$HOME/.ansible.cfg"
+mkdir "$HOME/.atom/" > /dev/null 2>&1
+ln -fs "$DOT_FILES/bash/bash_profile" "$HOME/.bash_profile"
+ln -fs "$DOT_FILES/bash/bashrc" "$HOME/.bashrc"
+ln -fs "$DOT_FILES/editorconfig/editorconfig" "$HOME/.editorconfig"
+ln -fs "$DOT_FILES/atom/config.cson" "$HOME/.atom/config.cson"
+ln -fs "$DOT_FILES/ansible/ansible.cfg" "$HOME/.ansible.cfg"
