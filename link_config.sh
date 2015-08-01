ccc82d865e78ee455438f2fb96dd1ea400e33a92
Gary Johnson <garycjohnso@gmail.com>
2015-08-01 01:18:23 -0400

    added ansible log

--- link_config.sh
+++ link_config.sh
@@ -2,6 +2,16 @@
 
 DOT_FILES="$HOME/.dotfiles"
 
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+sudo touch /var/log/ansible.log
+sudo chown johnsoga:staff /var/log/ansible.log
+
 ### Link Bash Files & Misc Config Files
 mkdir "$HOME/.atom/" > /dev/null 2>&1
 ln -fs "$DOT_FILES/bash/bash_profile" "$HOME/.bash_profile"
